65af121cb6493e3f6655e408859529695ae72b27
NiravDhanani <nirav.devotee@gmail.com>
2024-07-15 13:35:27 +0530

    task done

--- Practical/Extra_Task/12_Teaxt analysis/Public/js/index.js
+++ Practical/Extra_Task/12_Teaxt analysis/Public/js/index.js
@@ -1,14 +1,13 @@
 function SubmitString() {
-  // let str = document.querySelector("#textInput").value
-  let str = "document hello world chek hello"
+  let str = document.querySelector("#textInput").value
+  // let str = "document hello world chek hello"
   let result = document.querySelector("#result")
   let wordFrequency = document.querySelector("#wordFrequency")
   let WorldAveragelength = document.querySelector("#WorldAveragelength")
   let wordCount = document.querySelector("#wordCount")
   let commonWord = document.querySelector('#commonWord')
-  let regex = /[^a-zA-Z]/gi
 
-  str = str.replace(regex, ' ')
+
 
   result.innerHTML = `<p>Your Sentence :- <b> ${str} </b> </p>`
 
@@ -24,7 +23,7 @@ function SubmitString() {
   })
 
   let averagelength = totalwordlength / wordlength.length
-  WorldAveragelength.innerHTML = `<p>Word Average Length is :- <b> ${averagelength} </b> Words</p>`
+  WorldAveragelength.innerHTML = `<p>Word Average Length is :- <b> ${(averagelength).toFixed(2)} </b> Words</p>`
 
   let common = {}
   let mostCommonWord = str.split(" ")
@@ -37,11 +36,6 @@ function SubmitString() {
     }
   }
 
-  // for(let i in common){
-  //   wordCount.innerHTML = `<p>word counting :-  there are <b> ${common[i]}  ${i}</b> Words</p>`
-  // }
-
-
   let max = 0
   let maxword = ""
   for (let i in common) {
@@ -51,49 +45,4 @@ function SubmitString() {
     }
   }
   commonWord.innerHTML = `<p>Most Common word :-  <b> ${maxword} </b> </p>`
-}
-SubmitString()
-function SubmitString() {
-  let str = document.querySelector("#textInput").value;
-  let result = document.querySelector("#result");
-  let wordFrequency = document.querySelector("#wordFrequency");
-  let WorldAveragelength = document.querySelector("#WorldAveragelength");
-  let wordCount = document.querySelector("#wordCount");
-  let commonWord = document.querySelector('#commonWord');
-  let regex = /[^a-zA-Z\s]/gi;
-
-  str = str.replace(regex, ' ').replace(/\s+/g, ' ').trim();
-
-  result.innerHTML = `<p>Your Sentence :- <b>${str}</b></p>`;
-
-  let words = str.split(" ").filter(word => word.length > 0);
-  let wordFreq = words.length;
-
-  wordFrequency.innerHTML = `<p>Word Frequency :- there are <b>${wordFreq}</b> Words</p>`;
-
-  let wordLengths = words.map(item => item.length);
-  let totalWordLength = wordLengths.reduce((a, b) => a + b, 0);
-  let averageLength = (wordFreq > 0) ? (totalWordLength / wordLengths.length).toFixed(2) : 0;
-  
-  WorldAveragelength.innerHTML = `<p>Word Average Length is :- <b>${averageLength}</b> Words</p>`;
-
-  let common = {};
-  words.forEach(word => {
-    common[word] = (common[word] || 0) + 1;
-  });
-
-  let wordCountContent = Object.entries(common)
-    .map(([word, count]) => `<p>Word count :- there are <b>${count}</b> of <b>${word}</b> Words</p>`)
-    .join("");
-  wordCount.innerHTML = wordCountContent;
-
-  let max = 0;
-  let maxWord = "";
-  for (let word in common) {
-    if (common[word] > max) {
-      max = common[word];
-      maxWord = word;
-    }
-  }
-  commonWord.innerHTML = `<p>Most Common word :- <b>${maxWord}</b> </p>`;
 }
