562651f091661654385cd510d7943c7e8b94447a
NiravDhanani <nirav.devotee@gmail.com>
2024-07-15 09:31:14 +0530

    Bacis task file

---
+++ Theory/01_Basic/Variables.js
@@ -0,0 +1,21 @@
+//  syntaxt 
+
+console.log("hello wolrd");
+
+// variable
+
+let a  = 10 
+
+console.log(`this is let variabe ${a}`);
+a = 25
+console.log(`this is let variabe that's changable ${a}`);
+
+var b = 10
+console.log(`this is var variabe ${b}`);
+b=30;
+console.log(`this is var that's changable variabe ${b}`);
+
+const c = 5
+console.log(`this is const variable ${c}`);
+// once const variable declare that value  not  change
+
