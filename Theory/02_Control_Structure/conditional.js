bf3d9b3f76278519e095bdfd63f6b8a638ea8c0d
NiravDhanani <nirav.devotee@gmail.com>
2024-07-16 15:52:04 +0530

    make some changes

--- Theory/02_Control_Structure/conditional.js
+++ Theory/02_Control_Structure/conditional.js
@@ -52,3 +52,31 @@ switch (a) {
 
 console.log("===========================");
 
+let mark = 95;
+
+switch (true) {
+    case mark >= 90:
+        console.log("A+ Gread");
+        break;
+    case mark >= 80 && mark < 90:
+        console.log("A Gread");
+        break;
+    case mark >= 70 && mark < 80:
+        console.log("B+ Gread");
+        break;
+    case mark >= 60 && mark < 70:
+        console.log("B Gread");
+        break;
+    case mark >= 50 && mark < 60:
+        console.log("c Gread");
+        break;
+    case mark >= 40 && mark < 50:
+        console.log("D Gread");
+        break;
+    case mark >= 30 && mark < 40:
+        console.log("Fail");
+        break;
+    default : {
+        console.log("F Gread");
+    }
+}
