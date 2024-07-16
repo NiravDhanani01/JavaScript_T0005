06010b5838863c92f884075b47078c965b63b2a9
NiravDhanani <nirav.devotee@gmail.com>
2024-07-16 14:23:34 +0530

    change in folder structure

--- Practical/01_2 String/string.js
+++ Practical/01_2 String/string.js
@@ -3,6 +3,7 @@ let str = "Hello Good Morning 123"
 // char Count  
 let charCount = str.trim().length
 console.log("Here Total character is =>", charCount);
+console.log("======");
 
 // only words not number 
 let regex = /[^a-zA-Z]/gi
@@ -15,7 +16,6 @@ let words = str.split(" ")
 console.log("Words count");
 console.log(words.length);
 console.log("======");
-
 
 // frequency of obj 
 let frequency = {}
