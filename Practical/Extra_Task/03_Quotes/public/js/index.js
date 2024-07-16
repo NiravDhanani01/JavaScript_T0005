855c59c1b218690fe4d553870919d5fc2d17fa38
NiravDhanani <nirav.devotee@gmail.com>
2024-07-16 11:50:19 +0530

    set box shadow

--- Practical/Extra_Task/03_Quotes/public/js/index.js
+++ Practical/Extra_Task/03_Quotes/public/js/index.js
@@ -2,8 +2,7 @@ async function getQuote() {
     try {
         let response = await fetch("https://type.fit/api/quotes")
         let data = await response.json()
-        console.log(data);
-
+        
         let result = document.querySelector("#result")
         let auth = document.querySelector("#author")
         let id = Math.floor(Math.random() * data.length)
